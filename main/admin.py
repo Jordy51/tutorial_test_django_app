ef013eb663088302e615fc9dc2e867daf8d1fd69
Jordy <adityakumawat51@gmail.com>
2020-02-07 23:45:24 +0530

    Part 9

--- main/admin.py
+++ main/admin.py
@@ -1,20 +1,23 @@
 from tinymce.widgets import TinyMCE
 from django.contrib import admin
 from django.db import models
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 # Register your models here.
 
 
 class TutorialAdmin(admin.ModelAdmin):
     fieldsets = [
-        ("Title/date",
-         {"fields": ["tutorial_title", "tutorial_published"]}),
-        ("Content",
-         {"fields": ["tutorial_content"]})
+        ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("URL", {'fields': ["tutorial_slug"]}),
+        ("Series", {'fields': ["tutorial_series"]}),
+        ("Content", {"fields": ["tutorial_content"]})
     ]
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()}
     }
 
+
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial, TutorialAdmin)
--- main/models.py
+++ main/models.py
@@ -3,11 +3,13 @@ from datetime import datetime
 
 
 class TutorialCategory(models.Model):
+
     tutorial_category = models.CharField(max_length=200)
     category_summary = models.CharField(max_length=200)
-    category_slug = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200, default=1)
 
     class Meta:
+        # Gives the proper plural name for admin
         verbose_name_plural = "Categories"
 
     def __str__(self):
@@ -16,10 +18,12 @@ class TutorialCategory(models.Model):
 
 class TutorialSeries(models.Model):
     tutorial_series = models.CharField(max_length=200)
+
     tutorial_category = models.ForeignKey(TutorialCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
     series_summary = models.CharField(max_length=200)
 
     class Meta:
+        # otherwise we get "Tutorial Seriess in admin"
         verbose_name_plural = "Series"
 
     def __str__(self):
@@ -29,9 +33,9 @@ class TutorialSeries(models.Model):
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("date published", default=datetime.now())
+    tutorial_published = models.DateTimeField('date published')
+    #https://docs.djangoproject.com/en/2.1/ref/models/fields/#django.db.models.ForeignKey.on_delete
     tutorial_series = models.ForeignKey(TutorialSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
     tutorial_slug = models.CharField(max_length=200, default=1)
-
     def __str__(self):
         return self.tutorial_title
