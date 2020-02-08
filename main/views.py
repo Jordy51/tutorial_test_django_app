b6e43a62805775c272cf76817b15e795e93580cc
Jordy <adityakumawat51@gmail.com>
2020-02-08 12:31:00 +0530

    Part 10

--- main/views.py
+++ main/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
 
-# Create your views here.
+
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(
+            tutorial_category__category_slug=single_slug)
+
+        series_urls = {}
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(
+                tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request, "main/category.html", {"part_ones": series_urls})
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        return HttpResponse(f"{single_slug} is a tutorial!!!")
+
+    return HttpResponse(f"{single_slug} does not correspond to anything.")
 
 
 def homepage(request):
-    return render(request=request, template_name="main/home.html", context={"tutorials": Tutorial.objects.all})
+    return render(request=request, template_name="main/categories.html", context={"categories": TutorialCategory.objects.all})
+
 
 def register(request):
     if request.method == "POST":
@@ -51,7 +71,6 @@ def login_request(request):
                 messages.error(request, "Invalid username or password")
         else:
             messages.error(request, "Invalid username or password")
-
 
     form = AuthenticationForm()
     return render(request, "main/login.html", {"form": form})
